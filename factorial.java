1ac4f3fc1e82f5dd32e435ac9442ade194a88d42
Boggula.Chandra Sainadh Reddy <124142992+chandrasainadhreddy@users.noreply.github.com>
2023-12-19 14:23:36 +0530

    Add files via upload

---
+++ factorial.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+class factorial
+{
+  public static void main(String args[])
+  {
+     Scanner sc = new Scanner(System.in);
+     System.out.println("enter the number");
+     int n = sc.nextInt();
+     int fact=1,i;
+     for(i=1;i<=n;i++)
+     {
+        fact=fact*i;
+     }
+     System.out.println("the factorial of"+n+"is:"+fact);
+  }
+}
--- reverse.java
+++ reverse.java
@@ -1,19 +1,20 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 class reverse
 {
-   public static void main(String[]args)
+   public static void main(String args[])
    {
-       String a,b="";
-       int i,d;
-       Scanner sc = new Scanner(System.in);
-       System.out.println(" enter the original word: ");
-       a=sc.next();
-       d=a.length();
-       for(i=d-1;i>=0;i--)
-       {
-            b=b+a.charAt(i);
-       }
-       System.out.println("the reversed word is : "+b);
-   }
+     String a,b="";
+     int i,d=0;
+     Scanner sc = new Scanner(System.in);
+     System.out.println("enter the string             :");
+     a = sc.next();
+     d = a.length();
+     for(i=d-1;i>=0;i--)
+     {
+       b=b+a.charAt(i);
+     }
+     System.out.println("the string before reversing is : "+a);
+     System.out.println("the string after reversing is : " +b);
+  }
 }
